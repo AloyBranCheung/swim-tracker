26d2364c7178ace13c4fbbf3e0b82686ffa0f6aa
bcheung <brandon.cheung.dev@gmail.com>
2024-03-06 23:59:56 -0500

    add hot reload; fix stop script

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  //   // docker hotreload
-  //   webpack: (config) => {
-  //     config.watchOptions = {
-  //       poll: 1000,
-  //       aggregateTimeout: 300,
-  //     };
-  //     return config;
-  //   },
+  // docker hotreload
+  webpack: (config) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
