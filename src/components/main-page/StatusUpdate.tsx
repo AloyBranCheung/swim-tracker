0a990c3fb4f3cfd879437838f3fe37ccbf1761a4
bcheung <brandon.cheung.dev@gmail.com>
2024-03-10 15:47:02 -0400

    add animations

--- src/components/main-page/StatusUpdate.tsx
+++ src/components/main-page/StatusUpdate.tsx
@@ -31,7 +31,7 @@ export default function StatusUpdate() {
     <CardContainer className='transition-all duration-300' ref={containerRef} onMouseDown={() => setIsFocused(true)}>
       <div className="w-full h-full flex flex-col gap-2">
         <Textarea onChange={handleChange} value={value} label="Say something witty..." className={isFocused ? '' : "p-0 min-h-0 h-9 overflow-hidden text-opacity-0"} labelClassName={isFocused ? '' : "peer-placeholder-shown:top-1 text-lg top-1"} />
-        <Button className={`transition-all duration-300 w-20 self-end ${isFocused ? '' : 'hidden'}`} onClick={handleClickSubmit}>Submit</Button>
+        <Button className={`w-20 self-end ${isFocused ? '' : 'hidden'}`} onClick={handleClickSubmit}>Submit</Button>
       </div>
     </CardContainer>
   );
--- src/containers/403.tsx
+++ src/containers/403.tsx
@@ -1,4 +1,5 @@
 import Gutter from "@/components/Gutter";
+import Ticker from "@/components/Ticker403";
 import React from "react";
 
 export default function Page403() {
@@ -6,7 +7,7 @@ export default function Page403() {
     <div className="flex justify-center items-center w-[100vw] h-[100vh] bg-app-gradient">
       <Gutter className="overflow-hidden relative p-4">
         <div className="flex items-center justify-center">
-          <div className="w-96 h-full flex flex-col items-center justify-center gap-2 bg-gray-600 bg-opacity-15 rounded-2xl shadow-2xl p-4">
+          {/* <div className="w-96 h-80 flex flex-col items-center justify-center gap-2 bg-gray-600 bg-opacity-15 rounded-2xl shadow-2xl p-4 overflow-hidden">
             {new Array(5).fill(0).map((_, i) => (
               <h1
                 key={i}
@@ -15,12 +16,13 @@ export default function Page403() {
                 Access Denied
               </h1>
             ))}
-          </div>
+          </div> */}
+          <Ticker />
         </div>
 
         <a
           href="/api/auth/login"
-          className="text-white hover:text-gray-400 font-bold absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] bg-loading-gradient hover:bg-403-btn-gradient-hover p-2 px-16 rounded-2xl shadow-2xl hover:transition-all"
+          className="text-white hover:text-gray-400 font-bold absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] bg-loading-gradient hover:bg-403-btn-gradient-hover p-2 px-16 rounded-2xl shadow-2xl hover:transition-all hover:scale-110"
         >
           Login
         </a>
