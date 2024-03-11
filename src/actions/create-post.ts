9d92cc24cf8434df70589bab2b9035deb41161af
bcheung <brandon.cheung.dev@gmail.com>
2024-03-11 14:34:41 -0400

    add server action to add post to db

--- src/actions/create-post.ts
+++ src/actions/create-post.ts
@@ -1,6 +1,8 @@
 'use server'
 import { PostStatusSchema } from "@/validators/status-post"
 import Pino from 'pino';
+import prisma from '@/libs/prisma-client'
+import { getSession } from '@auth0/nextjs-auth0'
 
 const logger = Pino();
 
@@ -8,9 +10,9 @@ export type FormState = {
     msg: FormDataEntryValue | null;
     errors: {
         msg?: string[] | undefined
-    }
+    };
+    success: boolean;
 }
-
 
 export default async function createPost(prevState: FormState, formData: FormData): Promise<FormState> {
     const rawFormData: FormState = { ...prevState, msg: formData.get('msg') }
@@ -20,25 +22,39 @@ export default async function createPost(prevState: FormState, formData: FormDat
     if (!validatedField.success) {
         return {
             msg: formData.get('msg'),
-            errors: validatedField.error.flatten().fieldErrors
+            errors: validatedField.error.flatten().fieldErrors,
+            success: false
         }
     }
 
-    // TODO: user authentication pre-mutate data 
     try {
+        const session = await getSession();
+        if (!session?.user) return { errors: { msg: ['Unauthorized'] }, msg: formData.get('msg'), success: false }
 
+        // get user from our own db
+        const user = await prisma.user.findUnique({
+            where: {
+                auth0Id: session.user.sub
+            }
+        })
+        if (!user) return { errors: { msg: ['Error finding user'] }, msg: formData.get('msg'), success: false }
+        await prisma.statusPost.create({
+            data: { msg: validatedField.data.msg, userId: user.id }
+        })
     } catch (error) {
         logger.error(error);
         return {
             msg: formData.get('msg'),
             errors: {
                 msg: ['Error creating post.']
-            }
+            },
+            success: false
         }
     }
-
     // reset
     return {
-        msg: '', errors: {}
+        msg: '',
+        errors: {},
+        success: true
     }
 }
