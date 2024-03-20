e0f0cf005cf4ead1b294aa1102f9dec1b42e68c2
bcheung <brandon.cheung.dev@gmail.com>
2024-03-20 18:17:13 -0400

    wip: add swim program

---
+++ src/components/icons/CrossIcon.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Image from "next/image";
+
+interface CrossIconProps {
+  onClick?: React.MouseEventHandler<HTMLImageElement>;
+  style?: React.CSSProperties;
+}
+
+export default function CrossIcon({ onClick, style }: CrossIconProps) {
+  return (
+    <Image
+      style={style}
+      onClick={onClick}
+      src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAv0lEQVR4nO2VXQrCMBAGxwtalZY86NkVLP6hN6hQCaQgJdXdsFHEDOQt+02TbBooFP6JBrgBB2ChqFsCJ+AK1FrpLBT2YXSAE9S5MHeo8xlqLk8BEvlY6sc5RbyKBE3JY9IuZCRRRwLvwGbUC7E561SpRJ5N+m4rpUdhvvJsK5XKs0qZONNYw31E2ueUu280V/Piykjuubl0wFxeGfwyfYaa1uCR2KeIjwbPos9QU4Uv3iq3zM/dhdp5irhQ+E0ekyummbane5EAAAAASUVORK5CYII="
+      width={24}
+      height={24}
+      alt="exit icon"
+    />
+  );
+}
---
+++ src/containers/plans-page/SwimCategory.tsx
@@ -0,0 +1,46 @@
+"use client";
+import React, { useState } from "react";
+import { motion } from "framer-motion";
+import { Prisma, ProgramLevel } from "@prisma/client";
+// components
+import Card from "@/components/Card";
+import Modal from "@/components/Modal";
+
+type Programs = Prisma.SwimCategoryGetPayload<{
+  include: { programs: { include: { swimExercise: true } } };
+}>;
+
+interface SwimCategoryProps {
+  categoryName: Programs["category"];
+  programs: Programs["programs"];
+}
+
+export default function SwimCategory({
+  programs,
+  categoryName,
+}: SwimCategoryProps) {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <motion.div
+      whileHover={{ scale: 1.01 }}
+      whileTap={{ scale: 0.99 }}
+      onClick={() => setIsOpen(true)}
+    >
+      <Card
+        className={`flex cursor-pointer flex-col gap-2 duration-300 ${categoryName === ProgramLevel.BEGINNER && "bg-beginner-swim"}`}
+      >
+        <h2 className="text-2xl">{categoryName}</h2>
+        {categoryName === ProgramLevel.BEGINNER && (
+          <ul className="list-disc pl-4">
+            <li>I&apos;m getting back into it</li>
+            <li>I want to build some swim stamina and skills</li>
+            <li>Starting from 500m</li>
+          </ul>
+        )}
+      </Card>
+      <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+        test
+      </Modal>
+    </motion.div>
+  );
+}
