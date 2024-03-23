6ed25dad1c77bdf8ed415cff50a71b8ee9af1e53
bcheung <brandon.cheung.dev@gmail.com>
2024-03-22 23:00:45 -0400

    remove storybook boilerplate

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,6 @@
 import type { Preview } from "@storybook/react";
+// https://storybook.js.org/recipes/tailwindcss#2-provide-tailwind-to-stories
+import '../src/app/globals.css'
 
 const preview: Preview = {
   parameters: {
