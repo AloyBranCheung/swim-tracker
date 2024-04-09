25f243f38b26e63a3297886b025ec61f7e83d52b
bcheung <brandon.cheung.dev@gmail.com>
2024-04-09 10:44:03 -0400

    add authguard test

--- src/auth/AuthGuard.tsx
+++ src/auth/AuthGuard.tsx
@@ -14,6 +14,7 @@ export default async function AuthGuard({ children }: AuthGuardProps) {
   const session = await getServerSession();
 
   if (!session) {
+    console.log("403");
     return <Page403 />;
   }
 
