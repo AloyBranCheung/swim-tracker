b50e53a6632172407e1ac3a247e81d9352757eef
bcheung <brandon.cheung.dev@gmail.com>
2024-04-09 10:44:03 -0400

    add authguard test

---
+++ src/tests/authguard.test.tsx
@@ -0,0 +1,40 @@
+import { describe, it, expect, vi } from "vitest";
+import { render, screen } from "@testing-library/react";
+// components
+import AuthGuard from "@/auth/AuthGuard";
+
+vi.mock("../auth/auth-helper.ts");
+
+describe("helloworld test", () => {
+  it("should render helloworld", async () => {
+    const authHelper = await import("../auth/auth-helper");
+    authHelper.auth = vi.fn().mockReturnValue({
+      user: {
+        email: "test@test.com",
+      },
+    });
+
+    render(
+      await AuthGuard({
+        children: <div>helloworld</div>,
+      }),
+    );
+
+    await expect(screen.getByText("helloworld")).toBeDefined();
+  });
+
+  it("should NOT render helloworld", async () => {
+    const authHelper = await import("../auth/auth-helper");
+    authHelper.auth = vi.fn().mockReturnValue(null);
+
+    render(
+      await AuthGuard({
+        children: <div>helloworld</div>,
+      }),
+    );
+
+    await expect(screen.getByText("Login")).toBeDefined();
+    await expect(screen.getAllByText("Access Denied").length).toBe(5);
+    await expect(screen.getAllByText("Access Denied")).toBeDefined();
+  });
+});
