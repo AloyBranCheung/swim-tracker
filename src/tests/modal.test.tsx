64b52942cd79cd05785b54c31571255aab1254f0
bcheung <brandon.cheung.dev@gmail.com>
2024-04-10 11:04:29 -0400

    add modal test

---
+++ src/tests/modal.test.tsx
@@ -0,0 +1,40 @@
+import { expect, afterEach, describe, it, vi, beforeAll } from "vitest";
+import { cleanup, render, screen } from "@testing-library/react";
+// components
+import Modal from "@/components/Modal";
+
+describe("test Modal component", () => {
+  beforeAll(() => {
+    HTMLDialogElement.prototype.show = vi.fn();
+    HTMLDialogElement.prototype.showModal = vi.fn();
+    HTMLDialogElement.prototype.close = vi.fn();
+  });
+
+  afterEach(() => {
+    cleanup(); // remove duplicate renders
+  });
+
+  it("should render modal", () => {
+    render(
+      <div>
+        <Modal isOpen={true} onClose={() => {}}>
+          <div>helloworld</div>
+        </Modal>
+      </div>,
+    );
+
+    expect(screen.queryByText("helloworld")).not.toBeNull();
+  });
+
+  it("should not show modal contents", () => {
+    render(
+      <div>
+        <Modal isOpen={false} onClose={() => {}}>
+          <div>helloworld</div>
+        </Modal>
+      </div>,
+    );
+
+    expect(screen.queryByText("helloworld")).toBeNull();
+  });
+});
