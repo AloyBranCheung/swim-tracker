d31bca526a5d0045eafabd6f84cbb4887646a701
bcheung <brandon.cheung.dev@gmail.com>
2024-04-10 10:50:00 -0400

    add statusupdate frontend ui tests

--- src/tests/statusupdate.test.tsx
+++ src/tests/statusupdate.test.tsx
@@ -1,5 +1,5 @@
-import { describe, it, expect, vi } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { describe, it, expect, vi, afterEach, beforeEach } from "vitest";
+import { fireEvent, render, screen, cleanup } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 // components
 import StatusUpdate from "@/components/StatusUpdate";
@@ -25,11 +25,43 @@ vi.mock("react-dom", () => ({
 }));
 
 describe("test statusupdate component", () => {
+  afterEach(() => {
+    cleanup(); // remove duplicate renders
+  });
+
   it("should count msg.length (10) /280 correctly", async () => {
     render(<StatusUpdate />);
 
     await user.type(screen.getByRole("textbox"), "test typed");
+    const form = screen.getByTestId("statusupdateform");
+
+    fireEvent.submit(form);
 
     expect(screen.queryByText("10/280")).not.toBeNull();
+  });
+
+  it("should show validation err msg", async () => {
+    const mockedReactdom = await import("react-dom");
+    // server returns err msg
+    mockedReactdom.useFormState = vi.fn(() => [
+      {
+        msg: "",
+        errors: { msg: ["String must contain at least 1 character(s)"] },
+        success: false,
+      },
+      mockedAction,
+      false,
+    ]);
+
+    render(<StatusUpdate />);
+
+    await user.click(screen.getByRole("textbox"));
+
+    expect(screen.queryByText("0/280")).not.toBeNull();
+    expect(
+      screen.queryByText("String must contain at least 1 character(s)"),
+    ).not.toBeNull();
+
+    screen.debug();
   });
 });
