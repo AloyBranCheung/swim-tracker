b3c08f6d45cc173b58b4c7e22c88721183f2f6fa
bcheung <brandon.cheung.dev@gmail.com>
2024-04-10 23:05:36 -0400

    add intermediate program week 1

--- prisma/data-migrations/migrate.sh
+++ prisma/data-migrations/migrate.sh
@@ -1,7 +1,8 @@
-#!/usr/bin/env bash
+# !/usr/bin/env bash
 
 # to be run in root
 yarn run generate-client
 npx tsx ./prisma/data-migrations/migrate-users.ts
 npx tsx ./prisma/data-migrations/migrate-programs.ts
 npx tsx ./prisma/data-migrations/migrate-program-descriptions.ts
+npx tsx ./prisma/data-migrations/migrate-add-intermediate.ts
