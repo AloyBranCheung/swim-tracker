08478fa6ff5e2bd53afb2c4103cc762bd09765f5
brandon <brandon.cheung.dev@gmail.com>
2024-04-11 14:55:44 -0400

    add if no active journey ui flow; wip start overview card

--- src/app/journey/page.tsx
+++ src/app/journey/page.tsx
@@ -1,5 +1,38 @@
 import React from "react";
+import { redirect } from "next/navigation";
+// db
+import prisma from "@/libs/prisma-client";
+import getUserAction from "@/auth/get-user-action";
+// components
+import OverviewCard from "@/containers/journey-page/OverviewCard";
+import StartJourney from "@/containers/journey-page/StartJourney";
 
-export default function JourneyPage() {
-  return <div className="text-header-font">JourneyPage</div>;
+export default async function JourneyPage() {
+  const userDetails = await getUserAction();
+
+  if (!userDetails) {
+    redirect("/");
+  }
+
+  const activeJourney = await prisma.journey.findFirst({
+    where: {
+      userId: userDetails.dbUsr.id,
+      isActive: true,
+    },
+    include: {
+      swimCategory: true,
+    },
+  });
+
+  return (
+    <div className="flex flex-col gap-2">
+      {!activeJourney ? (
+        <StartJourney />
+      ) : (
+        <>
+          <OverviewCard url={activeJourney?.swimCategory.url} />
+        </>
+      )}
+    </div>
+  );
 }
