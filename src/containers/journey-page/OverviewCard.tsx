e86366c9b5ebf1c2c372e7976aec5d6413861158
brandon <brandon.cheung.dev@gmail.com>
2024-04-11 15:09:45 -0400

    wip overview card

--- src/containers/journey-page/OverviewCard.tsx
+++ src/containers/journey-page/OverviewCard.tsx
@@ -1,18 +1,28 @@
 import React from "react";
 
 interface OverviewCardProps {
-  url: string | null | undefined;
+  url: string | undefined;
+  title: string | undefined;
+  programTotal: number | null;
 }
 
-export default function OverviewCard({ url }: OverviewCardProps) {
-  console.log(url);
+export default function OverviewCard({
+  url,
+  title,
+  programTotal,
+}: OverviewCardProps) {
   return (
     <div
+      className="flex items-center justify-between rounded-2xl p-2"
       style={{
         backgroundImage: `url(${url})`,
       }}
     >
-      OverviewCard
+      <h1 className="text-lg font-semibold text-header-font">{title}</h1>
+      <div className="flex flex-col items-end gap-2">
+        <p className="text-header-font">{`completed/${programTotal}`}</p>
+        <p className="text-header-font">completed%</p>
+      </div>
     </div>
   );
 }
