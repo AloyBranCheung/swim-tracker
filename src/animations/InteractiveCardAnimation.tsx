6c7e52121de834e3f7d025cff70fd9303e75433e
bcheung <brandon.cheung.dev@gmail.com>
2024-04-13 06:44:00 -0400

    add journey forward progression

--- src/animations/InteractiveCardAnimation.tsx
+++ src/animations/InteractiveCardAnimation.tsx
@@ -3,16 +3,18 @@ import { motion } from "framer-motion";
 
 interface InteractiveCardAnimationProps {
   children: React.ReactNode;
+  isDisabled?: boolean;
 }
 
 export default function InteractiveCardAnimation({
   children,
+  isDisabled,
 }: InteractiveCardAnimationProps) {
   return (
     <motion.div
       className="cursor-pointer"
-      whileHover={{ scale: 1.01 }}
-      whileTap={{ scale: 0.99 }}
+      whileHover={!isDisabled ? { scale: 1.01 } : undefined}
+      whileTap={!isDisabled ? { scale: 0.99 } : undefined}
     >
       {children}
     </motion.div>
