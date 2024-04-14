26f7ee8a2a33f9644a986bf4b7e7cde185e4615e
bcheung <brandon.cheung.dev@gmail.com>
2024-04-14 17:59:30 -0400

    add journey integration tests

--- src/tests/utils/resetdb.ts
+++ src/tests/utils/resetdb.ts
@@ -5,12 +5,17 @@ const prisma = new PrismaClient();
 const resetDb = async () => {
     await prisma.$transaction([
         prisma.journey.deleteMany(),
-        prisma.program.deleteMany(),
         prisma.statusPost.deleteMany(),
-        prisma.swimCategory.deleteMany(),
-        prisma.swimExercise.deleteMany(),
         prisma.user.deleteMany(),
         prisma.userSwimActivityLog.deleteMany(),
+        /**
+         * don't delete these because they are static data from
+         * data-migrations...maybe in the future when these tables are more
+         * dynamic
+         * prisma.program.deleteMany(), 
+         * prisma.swimCategory.deleteMany(),
+         * prisma.swimExercise.deleteMany(),
+         */
     ])
 }
 
