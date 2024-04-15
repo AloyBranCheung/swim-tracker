c8a4fe25108e17a31a9fabfdd8a1108a17c41ba6
bcheung <brandon.cheung.dev@gmail.com>
2024-04-15 12:25:01 -0400

    add journey page e2e test

--- src/tests/e2e/journey.spec.ts
+++ src/tests/e2e/journey.spec.ts
@@ -10,21 +10,32 @@ test.describe("test journey flow from login to completing one swim", () => {
     await loginPage.gotoLogin()
     await loginPage.login(userCredentials.email, userCredentials.password)
 
+    const JOURNEY_PAGE_URL = `${loginPage.url}/journey`
+    const PLANS_PAGE_URL = `${loginPage.url}/plans`
+
     await page.getByRole('link', { name: 'journey-icon Journey' }).click();
-    await page.waitForURL(`${loginPage.url}/journey`)
+    await page.waitForURL(JOURNEY_PAGE_URL)
 
-    // await page.getByRole('button', { name: 'Let\'s go' }).click();
-    // await page.getByRole('heading', { name: 'BEGINNER' }).click();
-    // await page.getByRole('button', { name: 'Start Journey' }).click();
-    // // TODO: opening modal not working 
-    // await page.locator('div').filter({ hasText: /^Week 1$/ }).nth(1).click();
-    // await page.getByRole('button', { name: 'I have done this today!' }).click();
+    await page.getByRole('button', { name: 'Let\'s go' }).click();
+    await page.waitForURL(PLANS_PAGE_URL)
 
-    // expect(page.getByText("Today's goal completed :)")).toBeDefined()
-    // expect(page.getByText("✔️")).toBeDefined()
-    // expect(page.getByText("2")).toBeDefined()
-    // expect(page.getByText("Warmup")).toBeDefined()
-    // expect(page.getByText("Total: 525m")).toBeDefined()
+    await page.getByRole('heading', { name: 'BEGINNER' }).click();
+    await page.getByRole('button', { name: 'Start Journey' }).click();
+    await page.waitForURL(JOURNEY_PAGE_URL)
+
+    // TODO: opening modal not working 
+    await page.locator('div').filter({ hasText: /^Week 1$/ }).nth(1).click();
+    await page.getByRole('button', { name: 'I have done this today!' }).click();
+
+    await page.reload()
+
+    await page.locator('div').filter({ hasText: /^Week 1$/ }).nth(1).click();
+
+    expect(page.getByText("Today's goal completed :)")).toBeDefined()
+    expect(page.getByText("✔️")).toBeDefined()
+    expect(page.getByText("2")).toBeDefined()
+    expect(page.getByText("Warmup")).toBeDefined()
+    expect(page.getByText("Total: 525m")).toBeDefined()
   })
 })
 
