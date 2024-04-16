46ed4aad7cad834f464ac39bdbfe320e038ed64f
brandon <brandon.cheung.dev@gmail.com>
2024-04-16 19:59:38 -0400

    fix unit and integration tests

--- src/tests/mocks/mockuser.ts
+++ src/tests/mocks/mockuser.ts
@@ -1,8 +1,9 @@
+import { User } from '@prisma/client'
 import mockSession from "./mocksession";
 const date = new Date();
 
-const mockUser = {
-    id: 1,
+const mockUser: User = {
+    id: 'testuuid',
     email: "test@test.com",
     name: "test",
     auth0Id: "auth0test|1234",
