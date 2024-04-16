c3898a1e2675ffa4c7e4f7a7d7d0058af733cf4b
bcheung <brandon.cheung.dev@gmail.com>
2024-04-16 07:58:18 -0400

    move tf to TST folder, add test user

---
+++ terraform/TST/variables.tf
@@ -0,0 +1,9 @@
+variable "test_user_pass" {
+    description = "user password for test user"
+    type = string 
+}
+
+variable "test_user_email" {
+    description = "user email for test user"
+    type = string 
+}
