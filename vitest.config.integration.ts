4c0e50000100565dbfe882a4374b1d85b584e5d1
bcheung <brandon.cheung.dev@gmail.com>
2024-04-17 08:33:51 -0400

    add test, fix script flag err

--- vitest.config.integration.ts
+++ vitest.config.integration.ts
@@ -1,9 +1,12 @@
 // vitest.config.integration.ts
 import { defineConfig } from 'vitest/config'
 import { fileURLToPath } from 'url'
+import react from "@vitejs/plugin-react"
 
 export default defineConfig({
+    plugins: [react()],
     test: {
+        environment: "jsdom",
         setupFiles: ['src/tests/setups/setup.ts'],
         include: ['src/tests/integration/**/*.test.ts', 'src/tests/integration/**/*.test.tsx'],
         poolOptions: {
