89f73a22f9295cc42186f2267280887c7d70f32d
bcheung <brandon.cheung.dev@gmail.com>
2024-04-17 18:50:37 -0400

    add signout btn

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,20 +2,13 @@ import Link from "next/link";
 import React from "react";
 // components
 import ProfileCard from "@/containers/profile-page/ProfileCard";
+import SignoutButton from "@/components/SignoutButton";
 
 export default function ProfilePage() {
   return (
-    <div>
+    <div className="flex flex-col gap-2">
       <ProfileCard />
-      <Link href="/" className="border-2 border-solid border-black">
-        Back to main page
-      </Link>
-      <a
-        href="/api/auth/signout"
-        className="absolute left-1/2 top-1/2 translate-x-[-50%] translate-y-[-50%] cursor-pointer rounded-2xl bg-loading-gradient p-2 px-16 font-bold text-white shadow-2xl hover:bg-primary-ui hover:text-gray-400 hover:transition-all"
-      >
-        tmp signout btn
-      </a>
+      <SignoutButton />
     </div>
   );
 }
