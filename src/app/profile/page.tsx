5d37ee508c4a57e21bd1f6566e34c5f01c87d461
bcheung <brandon.cheung.dev@gmail.com>
2024-04-19 08:24:20 -0400

    add profile card

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,14 +1,29 @@
 import React from "react";
 import getUserAction from "@/auth/get-user-action";
+import prisma from "@/libs/prisma-client";
 // components
 import ProfileCard from "@/containers/profile-page/ProfileCard";
 import SignoutButton from "@/components/SignoutButton";
 
 export default async function ProfilePage() {
   const user = await getUserAction();
+
+  const currActiveJourney = await prisma.journey.findFirst({
+    where: {
+      isActive: true,
+    },
+    include: {
+      swimCategory: true,
+      program: true,
+    },
+  });
+
   return (
     <div className="flex flex-col gap-4">
-      <ProfileCard auth0Usr={user?.auth0Usr} />
+      <ProfileCard
+        auth0Usr={user?.auth0Usr}
+        currActiveJourney={currActiveJourney}
+      />
       <div className="text-header-font">lifetime distance swam</div>
       <div className="text-header-font">
         activity log here: title, last swim
