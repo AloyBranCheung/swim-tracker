dfc81bad8708db4ac43294e0708da20ac0a5a225
bcheung <brandon.cheung.dev@gmail.com>
2024-04-20 11:26:28 -0400

    fix unit test

--- src/tests/mocks/mockswimactivity.ts
+++ src/tests/mocks/mockswimactivity.ts
@@ -1,10 +1,11 @@
 import { UserSwimActivityLog } from "@prisma/client"
+import dayjs from "dayjs"
 
 const mockSwimActivity: UserSwimActivityLog[] = [
     {
         id: "3a0fe8dd-2bc9-444a-858c-37d1166686bf",
-        createdAt: new Date(),
-        updatedAt: new Date(),
+        createdAt: dayjs(new Date()).subtract(1, 'day').toDate(),
+        updatedAt: dayjs(new Date()).subtract(1, 'day').toDate(),
         totalDistanceSwam: 750,
         unit: "m",
         userId: "testUserId",
