ebb404332f5967e8006a2a435801d6f3e2d7f211
bcheung <brandon.cheung.dev@gmail.com>
2024-04-20 10:33:05 -0400

    fix test?

--- src/tests/mocks/mockswimactivity.ts
+++ src/tests/mocks/mockswimactivity.ts
@@ -3,16 +3,16 @@ import { UserSwimActivityLog } from "@prisma/client"
 const mockSwimActivity: UserSwimActivityLog[] = [
     {
         id: "3a0fe8dd-2bc9-444a-858c-37d1166686bf",
-        createdAt: new Date("2024-04-19T01:40:47.585Z"),
-        updatedAt: new Date("2024-04-19T01:40:47.585Z"),
+        createdAt: new Date(),
+        updatedAt: new Date(),
         totalDistanceSwam: 750,
         unit: "m",
         userId: "testUserId",
     },
     {
         id: "34a55ff7-7c56-417c-94d5-fe9625963df6",
-        createdAt: new Date("2024-04-19T13:06:17.372Z"),
-        updatedAt: new Date("2024-04-19T13:06:17.372Z"),
+        createdAt: new Date(),
+        updatedAt: new Date(),
         totalDistanceSwam: 750,
         unit: "m",
         userId: "testUserId",
