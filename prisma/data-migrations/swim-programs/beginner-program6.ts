37e86dddd86fb69848fb3d328cf527773d1c3256
bcheung <brandon.cheung.dev@gmail.com>
2024-04-21 08:53:43 -0400

    add program 5, add program 6

--- prisma/data-migrations/swim-programs/beginner-program6.ts
+++ prisma/data-migrations/swim-programs/beginner-program6.ts
@@ -2,42 +2,43 @@ import SwimProgramBuilder from "../util/SwimProgramBuilder";
 import { Accessory, ExerciseType, ProgramLevel } from "@prisma/client";
 
 export const swimProgram = new SwimProgramBuilder()
-    .startBuildingProgram("Week 5", 5)
+    .startBuildingProgram("Week 6", 6)
+    .addExerciseToProgram({
+        exerciseType: ExerciseType.WARMUP,
+        sets: 8,
+        distance: 25,
+        unit: "m",
+        notes: "rest 20 sec. per 25m",
+    })
     .addExerciseToProgram({
         exerciseType: ExerciseType.WARMUP,
         sets: 4,
         distance: 25,
-        unit: "m",
-        notes: "easy pace",
+        unit: 'm',
+        accessory: Accessory.KICKBOARD,
+        notes: "rest 20 sec. per 25m",
     })
     .addExerciseToProgram({
         exerciseType: ExerciseType.MAINSET,
-        sets: 8,
+        sets: 6,
         distance: 25,
-        unit: "m",
-        notes: "Rest 20 seconds every 25m.",
-        accessory: Accessory.KICKBOARD
+        unit: 'm',
+        accessory: Accessory.PULLBUOY,
+        notes: "rest 20 sec. per 25m",
     })
     .addExerciseToProgram({
         exerciseType: ExerciseType.MAINSET,
         sets: 2,
         distance: 25,
-        unit: "m",
-        notes: "Fast. Rest 20 seconds after each 25m."
-    })
-    .addExerciseToProgram({
-        exerciseType: ExerciseType.MAINSET,
-        sets: 8,
-        distance: 25,
-        unit: "m",
-        notes: "Moderate. Rest 20 seconds after each 25m."
+        unit: 'm',
+        notes: "face pace, rest 30 sec. per 25m"
     })
     .addExerciseToProgram({
         exerciseType: ExerciseType.COOLDOWN,
-        sets: 5,
+        sets: 4,
         distance: 25,
         unit: 'm',
-        notes: 'chill vibes only '
+        notes: 'chill vibes only'
     })
     .addProgramToSwimProgram(ProgramLevel.BEGINNER) // add this line once all exercises are added
     .build();
