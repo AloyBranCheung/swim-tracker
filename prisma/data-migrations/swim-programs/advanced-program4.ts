d0531849f96eff4db1a3c8f936971f1737eb4135
bcheung <brandon.cheung.dev@gmail.com>
2024-04-21 10:05:45 -0400

    add advanced 4

---
+++ prisma/data-migrations/swim-programs/advanced-program4.ts
@@ -0,0 +1,35 @@
+import SwimProgramBuilder from "../util/SwimProgramBuilder";
+import { ExerciseType, ProgramLevel } from "@prisma/client";
+
+export const swimProgram = new SwimProgramBuilder()
+    .startBuildingProgram("Week 4", 4)
+    .addExerciseToProgram({
+        exerciseType: ExerciseType.WARMUP,
+        sets: 1,
+        distance: 100,
+        unit: "m",
+        notes: "easy",
+    })
+    .addExerciseToProgram({
+        exerciseType: ExerciseType.WARMUP,
+        sets: 4,
+        distance: 25,
+        unit: 'm',
+        notes: 'moderate @ 20 sec./25m'
+    })
+    .addExerciseToProgram({
+        exerciseType: ExerciseType.MAINSET,
+        sets: 3,
+        distance: 200,
+        unit: 'm',
+        notes: 'moderate @ 20 sec./200m'
+    })
+    .addExerciseToProgram({
+        exerciseType: ExerciseType.COOLDOWN,
+        sets: 1,
+        distance: 100,
+        unit: 'm',
+        notes: 'chill vibes only'
+    })
+    .addProgramToSwimProgram(ProgramLevel.ADVANCED) // add this line once all exercises are added
+    .build();
