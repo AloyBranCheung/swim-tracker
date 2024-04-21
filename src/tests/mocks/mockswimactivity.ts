9c94c25f79f85327f8f35157d70512203f99b00d
bcheung <brandon.cheung.dev@gmail.com>
2024-04-21 10:35:18 -0400

    fix unit test by mocking system time

--- src/tests/mocks/mockswimactivity.ts
+++ src/tests/mocks/mockswimactivity.ts
@@ -4,16 +4,16 @@ import dayjs from "dayjs"
 const mockSwimActivity: UserSwimActivityLog[] = [
     {
         id: "3a0fe8dd-2bc9-444a-858c-37d1166686bf",
-        createdAt: dayjs(new Date()).subtract(1, 'day').toDate(),
-        updatedAt: dayjs(new Date()).subtract(1, 'day').toDate(),
+        createdAt: dayjs(new Date('2024-03-24')).subtract(1, 'day').toDate(),
+        updatedAt: dayjs(new Date('2024-03-24')).subtract(1, 'day').toDate(),
         totalDistanceSwam: 750,
         unit: "m",
         userId: "testUserId",
     },
     {
         id: "34a55ff7-7c56-417c-94d5-fe9625963df6",
-        createdAt: new Date(),
-        updatedAt: new Date(),
+        createdAt: dayjs(new Date('2024-03-24')).subtract(2, 'day').toDate(),
+        updatedAt: dayjs(new Date('2024-03-24')).subtract(2, 'day').toDate(),
         totalDistanceSwam: 750,
         unit: "m",
         userId: "testUserId",
