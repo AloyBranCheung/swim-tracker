11c93067a78bc67e80c4fe5acd5e45cd83be460b
bcheung <brandon.cheung.dev@gmail.com>
2024-04-24 23:04:18 -0400

    add comments

--- src/app/(main)/template.tsx
+++ src/app/(main)/template.tsx
@@ -1,3 +1,6 @@
+// there is currently a bug with next14 app router
+// https://dev.to/joseph42a/nextjs-page-transition-with-framer-motion-33dg
+// https://stackoverflow.com/questions/77603249/how-to-make-a-page-transition-with-framer-motion-and-next-js-14
 import React from "react";
 import PageTransition from "@/animations/PageTransition";
 
